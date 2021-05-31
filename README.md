3693bd83c744da06d145f0998b7f34e9fb8a8c8b
衣川 亮太 <kinukawa@tombo-works.com>
2021-05-31 10:55:34 +0900

    Initial commit, start nerves_system_f3rp70 with based on nerves_system_rpi3

---
+++ README.md
@@ -0,0 +1,62 @@
+# F3RP70 Support
+
+This is the base Nerves System configuration for the F3RP70,
+[e-RT3 Products, OS-free CPU module](https://www.yokogawa.com/solutions/products-platforms/control-system/ert3-embedded-controller/ert3-products/ert3-products-cpu/).
+
+
+
+| Feature              | Description                                                 |
+| -------------------- | ----------------------------------------------------------- |
+| CPU                  | ARM Cortex-A9 MPCore (Dual) 866 MHz                         |
+| Memory               | 1 GB SDRAM                                                  |
+| Storage              | 256MB Flash and Two SD slots (SD1/SD2)                      |
+| Linux kernel         | 4.14.164 w/ 4.14.164-rt73 and ert3xlnx patch                |
+| IEx terminal         | UART `ttySP0`                                               |
+| GPIO, I2C, SPI       | No                                                          |
+| ADC                  | No                                                          |
+| PWM                  | No                                                          |
+| UART                 | ttySP0                                                      |
+| Display              | No                                                          |
+| Camera               | No                                                          |
+| Ethernet             | Two 1Gbps (eth0 and eth1)                                   |
+| WiFi                 | No                                                          |
+| Bluetooth            | No                                                          |
+| Audio                | No                                                          |
+| RTC                  | Yes w/ battery                                              |
+
+
+## Using
+
+This port currently only runs off a SD card.
+
+If you need custom modifications to this system for your device, clone this
+repository and update as described in [Making custom systems](https://hexdocs.pm/nerves/systems.html#customizing-your-own-nerves-system)
+
+
+## Networking
+
+The board has two 1 Gbps Ethernet interfaces. Here's an example `:vintage_net`
+configuration that enables both of them:
+
+```elixir
+config :vintage_net,
+  config: [
+    {"eth0",
+     %{
+       type: VintageNetEthernet,
+       ipv4: %{
+         method: :static,
+         address: "192.168.3.72",
+         prefix_length: 24,
+         gateway: "192.168.3.1",
+         name_servers: ["192.168.3.1"]
+       }
+     }},
+    {"eth1",
+     %{
+       type: VintageNetEthernet,
+       ipv4: %{method: :dhcp}
+     }}
+  ]
+```
+
---
+++ mix.exs
@@ -0,0 +1,139 @@
+defmodule NervesSystemF3RP70.MixProject do
+  use Mix.Project
+
+  @github_organization "pojiro"
+  @app :nerves_system_f3rp70
+  @source_url "https://github.com/#{@github_organization}/#{@app}"
+  @version Path.join(__DIR__, "VERSION")
+           |> File.read!()
+           |> String.trim()
+
+  def project do
+    [
+      app: @app,
+      version: @version,
+      elixir: "~> 1.6",
+      compilers: Mix.compilers() ++ [:nerves_package],
+      nerves_package: nerves_package(),
+      description: description(),
+      package: package(),
+      deps: deps(),
+      aliases: [loadconfig: [&bootstrap/1], docs: ["docs", &copy_images/1]],
+      docs: docs(),
+      preferred_cli_env: %{
+        docs: :docs,
+        "hex.build": :docs,
+        "hex.publish": :docs
+      }
+    ]
+  end
+
+  def application do
+    []
+  end
+
+  defp bootstrap(args) do
+    set_target()
+    Application.start(:nerves_bootstrap)
+    Mix.Task.run("loadconfig", args)
+  end
+
+  defp nerves_package do
+    [
+      type: :system,
+      artifact_sites: [
+        {:github_releases, "#{@github_organization}/#{@app}"}
+      ],
+      build_runner_opts: build_runner_opts(),
+      platform: Nerves.System.BR,
+      platform_config: [
+        defconfig: "nerves_defconfig"
+      ],
+      # The :env key is an optional experimental feature for adding environment
+      # variables to the crosscompile environment. These are intended for
+      # llvm-based tooling that may need more precise processor information.
+      env: [
+        {"TARGET_ARCH", "arm"},
+        {"TARGET_CPU", "cortex_a9"},
+        {"TARGET_OS", "linux"},
+        {"TARGET_ABI", "gnueabihf"}
+      ],
+      checksum: package_files()
+    ]
+  end
+
+  defp deps do
+    [
+      {:nerves, "~> 1.6.0 or ~> 1.7.4", runtime: false},
+      {:nerves_system_br, "1.15.2", runtime: false},
+      {:nerves_toolchain_armv7_nerves_linux_gnueabihf, "~> 1.4.2", runtime: false},
+      {:nerves_system_linter, "~> 0.4", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.22", only: :docs, runtime: false}
+    ]
+  end
+
+  defp description do
+    """
+    Nerves System - F3RP70
+    """
+  end
+
+  defp docs do
+    [
+      extras: ["README.md", "CHANGELOG.md"],
+      main: "readme",
+      source_ref: "v#{@version}",
+      source_url: @source_url,
+      skip_undefined_reference_warnings_on: ["CHANGELOG.md"]
+    ]
+  end
+
+  defp package do
+    [
+      files: package_files(),
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => @source_url}
+    ]
+  end
+
+  defp package_files do
+    [
+      "CHANGELOG.md",
+      "Config.in",
+      "LICENSE",
+      "README.md",
+      "VERSION",
+      "fwup-revert.conf",
+      "fwup.conf",
+      "fwup_include",
+      "kernel-patches",
+      "linux-4.14.164-rt73-ert3xlnx.defconfig",
+      "mix.exs",
+      "nerves_defconfig",
+      "post-build.sh",
+      "post-createfs.sh",
+      "rootfs_overlay",
+      "uEnv.txt"
+    ]
+  end
+
+  # Copy the images referenced by docs, since ex_doc doesn't do this.
+  defp copy_images(_) do
+    File.cp_r("assets", "doc/assets")
+  end
+
+  defp build_runner_opts() do
+    case System.get_env("BR2_PRIMARY_SITE") do
+      nil -> []
+      primary_site -> [make_args: ["BR2_PRIMARY_SITE=#{primary_site}"]]
+    end
+  end
+
+  defp set_target() do
+    if function_exported?(Mix, :target, 1) do
+      apply(Mix, :target, [:target])
+    else
+      System.put_env("MIX_TARGET", "target")
+    end
+  end
+end
